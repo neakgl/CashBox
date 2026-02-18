27ab7bf06931ecbb62698721254f94dd3c38dbf8
neakgl <eftalya.akgul@gmail.com>
2026-02-18 16:25:53 +0300

    Add project files.

---
+++ CashBox.Core/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿using CashBox.Core.Entities.Common;
+
+namespace CashBox.Core.Entities;
+
+public class Category : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+
+    public ICollection<Expense> Expenses { get; set; } = new List<Expense>();
+}
---
+++ CashBox.Core/Entities/Expense.cs
@@ -0,0 +1,14 @@
+﻿using CashBox.Core.Entities.Common;
+
+namespace CashBox.Core.Entities;
+
+public class Expense : BaseEntity
+{
+    public string Description { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+
+    //every expense has a category
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+}
