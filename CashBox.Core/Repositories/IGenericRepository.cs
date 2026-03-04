c522428933ab2ef4ba17926aae6ca8a93ed0101b
neakgl <eftalya.akgul@gmail.com>
2026-03-04 14:21:11 +0300

    Add generic repository and service interfaces to Core layer

---
+++ CashBox.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+
+namespace CashBox.Core.Repositories;
+
+public interface IGenericRepository<T> where T : class
+{
+    // Read İşlemleri
+    Task<T> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    IQueryable<T> Where(Expression<Func<T, bool>> expression); 
+
+    // Create İşlemi
+    Task AddAsync(T entity);
+
+    // Update İşlemi
+    void Update(T entity);
+
+    // Delete İşlemi
+    void Remove(T entity);
+}
---
+++ CashBox.Core/Services/IGenericService.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+
+namespace CashBox.Core.Services;
+
+public interface IGenericService<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+}
