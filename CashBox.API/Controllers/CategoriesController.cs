0fda31fac524ccce8d3e1de531b6bc481dc99415
neakgl <eftalya.akgul@gmail.com>
2026-03-10 17:18:29 +0300

    add expense and category controllers and configure Swagger UI auto-launch

---
+++ CashBox.API/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿using CashBox.Core.Entities;
+using CashBox.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoriesController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    [HttpPost("getAllCategory")]
+    public async Task<IActionResult> GetAll()
+    {
+        var categories = await _categoryService.GetAllAsync();
+        return Ok(categories);
+    }
+
+    [HttpPost("createCategory")]
+    public async Task<IActionResult> Create(Category category)
+    {
+        await _categoryService.AddAsync(category);
+
+        return Ok(category);
+    }
+
+    [HttpPost("updateCategory")]
+    public async Task<IActionResult> Update(Category category)
+    {
+        _categoryService.Update(category);
+        return Ok("Kategori güncellendi.");
+    }
+
+    [HttpPost("removeCategory")]
+    public async Task<IActionResult> Remove(int id)
+    {
+        var category = await _categoryService.GetByIdAsync(id);
+        if (category == null) return NotFound("Kategori bulunamadı.");
+
+        _categoryService.Remove(category);
+        return Ok("Kategori silindi.");
+    }
+}
---
+++ CashBox.API/Controllers/ExpensesController.cs
@@ -0,0 +1,46 @@
+﻿using CashBox.Core.Entities;
+using CashBox.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    private readonly IExpenseService _expenseService;
+
+    public ExpensesController(IExpenseService expenseService)
+    {
+        _expenseService = expenseService;
+    }
+
+    [HttpPost("getAllExpense")]
+    public async Task<IActionResult> GetAll()
+    {
+        var expenses = await _expenseService.GetAllAsync();
+        return Ok(expenses);
+    }
+
+    [HttpPost("createExpense")]
+    public async Task<IActionResult> Create(Expense expense)
+    {
+        await _expenseService.AddAsync(expense);
+        return Ok(expense);
+    }
+
+    [HttpPost("updateExpense")]
+    public async Task<IActionResult> Update(Expense expense)
+    {
+        _expenseService.Update(expense);
+        return Ok("Harcama güncellendi.");
+    }
+
+    [HttpPost("removeExpense")]
+    public async Task<IActionResult> Remove(int id)
+    {
+        var expense = await _expenseService.GetByIdAsync(id);
+        if (expense == null) return NotFound("Harcama bulunamadı.");
+
+         _expenseService.Remove(expense);
+        return Ok("Harcama silindi.");
+    }
+}
