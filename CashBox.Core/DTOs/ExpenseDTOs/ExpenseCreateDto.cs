327a9734a7c9b6f79f24641c454ccbfda5f65ae7
neakgl <eftalya.akgul@gmail.com>
2026-03-11 17:11:02 +0300

    fix: fix expense mapping bug

--- CashBox.Core/DTOs/ExpenseDTOs/ExpenseCreateDto.cs
+++ CashBox.Core/DTOs/ExpenseDTOs/ExpenseCreateDto.cs
@@ -2,7 +2,7 @@
 
 public class ExpenseCreateDto
 {
-    public string Title { get; set; }
+    public string Description { get; set; }
     public decimal Amount { get; set; }
     public DateTime? Date { get; set; }
     public int CategoryId { get; set; }
--- CashBox.Core/DTOs/ExpenseDTOs/ExpenseUpdateDto.cs
+++ CashBox.Core/DTOs/ExpenseDTOs/ExpenseUpdateDto.cs
@@ -3,7 +3,7 @@
 public class ExpenseUpdateDto
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+    public string Description { get; set; }
     public decimal Amount { get; set; }
     public DateTime Date { get; set; }
     public int CategoryId { get; set; }
